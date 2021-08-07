125a6b432a2dcd3da971957a89f5fe4e2cb018cc
Alibek2 <ali.nurbekov07@gmail.com>
2021-08-07 13:32:51 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> ArrayList1 = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Напиши 4 слов");
+        while
+        (ArrayList1.size() < 4 ) {
+            String str0 = scanner.nextLine();
+            ArrayList1.add(str0);
+        }
+        info(ArrayList1);
+
+        ArrayList<String> arrayList2 = new ArrayList<>();
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Напиши 4 слов");
+
+        while (arrayList2.size() < 4) {
+            String str0 = scanner1.nextLine();
+            arrayList2.add(str0);
+        }
+        info(arrayList2);
+
+        ArrayList<String> arrayList3 = new ArrayList<>();
+        Collections.reverse(arrayList2);
+        Iterator<String> iterator = arrayList2.iterator();
+        while
+        (iterator.hasNext()){
+            Iterator<String> iterator1 = ArrayList1.iterator();
+            while (iterator.hasNext()) {
+                arrayList3.add(iterator1.next());
+                iterator1.remove();
+                break;
+            }
+            arrayList3.add(iterator.next()); }
+        System.out.println(arrayList3);
+        System.out.println("Отсортирован");
+        arrayList3.sort(Comparator.comparing((String::length)));
+        System.out.println(arrayList3);
+    }
+    public static void info(ArrayList<?> arrayList) {
+        System.out.println(":::::::::::::::::::::::::");
+        Iterator<?> iterator = arrayList.iterator();
+        while
+        (iterator.hasNext()) {
+            System.out.println(iterator.next()); }
+
+    }
+}
